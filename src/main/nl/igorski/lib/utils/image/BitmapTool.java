572945b4cb6d9e78a1be4ef906a79b488da465ca
Igor Zinken <igor.zinken@igorski.nl>
2014-08-17 14:49:26 +0200

    moved namespace around, updated ignore

--- src/main/nl/igorski/lib/utils/image/BitmapTool.java
+++ src/main/nl/igorski/lib/utils/image/BitmapTool.java
@@ -20,7 +20,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package nl.igorski.lib.image;
+package nl.igorski.lib.utils.image;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
